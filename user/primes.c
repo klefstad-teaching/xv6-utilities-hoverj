c82e63a65b0ce3679d0a9fb274abb4855e6f477b
Jeff <hoverj@uci.edu>
2022-04-03 22:06:09 -0700

    Created the find and primes files. Attempted but not much progress

---
+++ user/primes.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+int findPrime(int t){
+	int primeList[] = {
+
+}
+
+
+int main(int argc, char *argv[]){
+
+  int *p = (int*)malloc(2*sizeof(int));
+
+
+exit(0);
+}//end of main
