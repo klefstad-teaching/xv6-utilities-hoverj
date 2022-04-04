28861f312a8c593428599a4132e3731daaf86632
Jeff <hoverj@uci.edu>
2022-04-03 22:43:34 -0700

    Added boolean func to determine if num was prime or not

--- user/primes.c
+++ user/primes.c
@@ -1,10 +1,16 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include <math.h>
 
-
-int findPrime(int t){
-	int primeList[] = {
+int isPrime(int i){
+  int upperbound = (int)sqrt(i);  
+  for(int x = 2; x <= upperbound; x++){
+	if(i%x == 0){
+	  return 0;
+	}
+  }//end of for loop
+  return 1;
 
 }
 
@@ -12,7 +18,13 @@ int findPrime(int t){
 int main(int argc, char *argv[]){
 
   int *p = (int*)malloc(2*sizeof(int));
+  
+  for(int x = 2; x <= 35; x++){
+  
+    if(isPrime(x) == 1){
+      //The numer is prime do something here
+    }//end of if
+  }//end of for loop
 
-
-exit(0);
+  exit(0);
 }//end of main
