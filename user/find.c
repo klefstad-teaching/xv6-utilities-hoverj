cf57e165a9bcb52716570e30a876342831348766
Jeff <hoverj@uci.edu>
2022-04-04 23:45:43 -0700

    Worked on find. I feel close. And got skeleton of primes

--- user/find.c
+++ user/find.c
@@ -1,9 +1,91 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/fs.h"
+
+//using fmtname from ls.h
+char* my_fmtname(char *path){
+  static char buf[DIRSIZ+1];
+  char* p;
+  for(p=path+strlen(path); p>= path && *p != '/'; p--)
+	  ;
+  p++;
+  //Return the padded name
+  if(strlen(p) >= DIRSIZ)
+	  return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
+}
+
+//rewriting ls to include the file comparison
+void my_find(char *path, char *fileCompare){
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+//  if((fd = open(path, 0)) < 0){
+//    fprintf(2, "ls: cannot open %s\n", path);
+//    return;
+ // }//end of if
+
+//  if(fstat(fd, &st) < 0){
+//    fprintf(2, "ls: cannot stat %s\n", path);
+//    close(fd);
+//    return;
+//  }//end of if
+
+  switch(st.type){
+  case T_FILE:
+	  printf("%s %d %d %l\n", my_fmtname(path), st.type, st.ino, st.size);
+	  break;
+
+  case T_DIR:
+	  if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
+	    printf("ls: path is too long");
+	    break;
+	  }
+	  strcpy(buf, path);
+	  p = buf + strlen(buf);
+	  *p++ = '/';
+	  while(read(fd, &de, sizeof(de)) == sizeof(de)){
+	    if(de.inum == 0)
+		    continue;
+
+	    if(strcmp(de.name, ".") == 0 || strcmp(de.name, ".."))//skip over the . and .. directores
+	      continue;
+	    
+	    memmove(p, de.name, DIRSIZ);
+	    p[DIRSIZ] = 0;
+
+	  // if(stat(buf, &st) < 0){
+	//	   printf("ls: cannot stat %s\n", buf);
+	//	   continue;
+	//   }
+	   if(st.type == T_DIR)
+		  my_find(buf, fileCompare);
+	   else if(st.type == T_FILE && strcmp(p, fileCompare) == 0)
+		   printf("%s\n", buf);
+
+	    
+	  }
+	  break;
+
+  }//end of switch statement
+  close(fd);
+
+}//end of ls rewrite
+
 
 int main(int argc, char *argv[]){
   
-
+  if(argc < 3){
+    printf("Incorrect input received");
+    exit(0);
+  }
+  char* startingPath = argv[1];
+  char* searchString = argv[2];
+  my_find(startingPath, searchString);
   exit(0);
 }
--- user/primes.c
+++ user/primes.c
@@ -1,11 +1,9 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include <math.h>
 
-int isPrime(int i){
-  int upperbound = (int)sqrt(i);  
-  for(int x = 2; x <= upperbound; x++){
+int isPrime(int i){ 
+  for(int x = 2; x < i; x++){
 	if(i%x == 0){
 	  return 0;
 	}
@@ -18,11 +16,29 @@ int isPrime(int i){
 int main(int argc, char *argv[]){
 
   int *p = (int*)malloc(2*sizeof(int));
-  
+  int *buffer = (int*)malloc(2*sizeof(int));
+  pipe(p);
+
   for(int x = 2; x <= 35; x++){
-  
+ 	int* intP = &x; 
     if(isPrime(x) == 1){
-      //The numer is prime do something here
+      if(fork() == 0){
+        read(p[0], buffer, 1);
+        printf("prime %d\n", buffer);
+
+        write(p[1], intP, 1);
+        close(p[1]);	
+      }//end of fork if
+    else{
+     write(p[1], intP, 1);
+     close(p[1]);
+     wait(0);
+
+     read(p[0],buffer, 1);
+     printf("prime %d\n", buffer);
+     close(p[0]);
+   
+    }//end of else
     }//end of if
   }//end of for loop
 
